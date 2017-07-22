f096ad99d7dff519326d1e0def2cc799c30e376c
BatenkovT <taras.batenkov@gmail.com>
2017-07-22 20:07:09 +0300

    Added eslint

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
     entry: './src/index.js',
     output: {
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, 'docs'),
         filename: 'bundle.js'
     },
     module: {
